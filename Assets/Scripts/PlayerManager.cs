d8469b69275bd013c5c2335c454089282e45af2a
chushoga <chushoga@gmail.com>
2016-12-13 13:43:08 +0900

    Input re-work
    re-did the input to allow for only moving when touched or clicked.
    Note: unfinished....

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,19 +7,9 @@ public class PlayerManager : MonoBehaviour
     /// <summary>
     /// 1 - The speed of the ship
     /// </summary>
-    public float speed = 1f;
-    private Vector2 speedVector = new Vector2(1f, 0f);
-
-    //The position you clicked
-    public Vector2 targetPosition;
-    //That position relative to the players current position (what direction and how far did you click?)
-    public Vector2 relativePosition;
-
-    // 2 - Store the movement
-    private Vector2 movement;
-
-    private Vector2 touchOrigin = -Vector2.one; //Used to store location of screen touch origin for mobile controls.
-
+    public float speed = 1f;    
+	private float halfScreen; // half of the screen width
+	private Vector3 clickPos; // clicked or touched position
 
     void Start()
     {
@@ -28,7 +18,7 @@ public class PlayerManager : MonoBehaviour
 		Debug.Log("Unity Editor");
 		#endif
 
-		#if UNITY_IOS
+		#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 		Debug.Log("Iphone");
 		#endif
 
@@ -40,66 +30,53 @@ public class PlayerManager : MonoBehaviour
 		Debug.Log("Stand Alone Windows");
 		#endif
 
-        speedVector.x = speed;
+		// set half screen
+		halfScreen = Screen.width / 2;
     }
 
     void Update()
     {
 
-        // check if we are running either in the Unity editor aor a standalone build
+        // check if we are running either in the Unity editor or a pc build
 
-		#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
+		#if UNITY_STANDALONE || UNITY_EDITOR
 
-		Debug.Log("standAlone");
         // 3 - Retrieve the mouse position
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // set click position
+
+			print("test" + halfScreen); // maybe dont need half the screen
+			print("mouse click pos: " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+			// check if greater thatn half the screen. move left if less than and right if greater than.
+			if (clickPos.x < 1f){
+				print("move left");
+			}
+			if (clickPos.x > 1f) {
+				print("move right");
+			}
+				
         }
 
-        //4 - Find the relative poistion of the target based upon the current position
-        // Update each frame to account for any movement
-		relativePosition = new Vector2(targetPosition.x - gameObject.transform.position.x, gameObject.transform.position.y);
-
-		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+		// check if mobile or not and if it is the use this input.
+		// this checks IOS, ANDROID, and WINDOWS mobile OS type.
+		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8
 
         // Check if Input has registered more than zero touches
-        if (Input.touchCount > 0)
-	    {
-			
-            //Store the first touch detected.
-            Touch myTouch = Input.touches[0];
-            
-            //Check if the phase of that touch equals Began
-            if (myTouch.phase == TouchPhase.Began)
-            {
-                //If so, set touchOrigin to the position of that touch
-				targetPosition = myTouch.position;
-               
-            }
-			relativePosition = new Vector2(targetPosition.x - gameObject.transform.position.x, gameObject.transform.position.y);
-        }
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary){
+			Vector2 touchPosition = Input.GetTouch(0).position;
+
+			// Check if it is left or right and go in that direction
+			if (touchPosition.x < halfScreen){
+				gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+			} else if(touchPosition.x > halfScreen){
+				gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+			}
+		}
 
 		#endif //End of mobile platform dependendent compilation section started above with #elif
 
     }
-
-    void FixedUpdate()
-    {
-        // 5 - If you are about to overshoot the target, reduce velocity to that distance
-        //      Else cap the Movement by a maximum speed per direction (x then y)
-
-        if (speedVector.x * Time.deltaTime >= Mathf.Abs(relativePosition.x))
-        {
-            movement.x = relativePosition.x;
-        }
-        else
-        {
-            movement.x = speedVector.x * Mathf.Sign(relativePosition.x);
-        }
-       
-        // 6 - Move the game object using the physics engine
-        GetComponent<Rigidbody2D>().velocity = movement;
-
-    }
+    
 }
