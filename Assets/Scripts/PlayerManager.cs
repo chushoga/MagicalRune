254247504b1a48a1fdb335e28af997d7ab060f09
chushoga <chushoga@gmail.com>
2016-12-21 16:57:28 +0900

    Some more work on the movement
    More work on input type. Thinking that left right will be ok but need to
    make more smooth movement. Also TODO: have the movement start from the
    start of touch and not stop till leave it. Figure out how to do this.
    Also check to see if the normal mouse input would work and/or if it is
    better.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -13,7 +13,7 @@ public class PlayerManager : MonoBehaviour
 
     void Start()
     {
-
+		/*
 		#if UNITY_EDITOR
 		Debug.Log("Unity Editor");
 		#endif
@@ -32,6 +32,7 @@ public class PlayerManager : MonoBehaviour
 
 		// set half screen
 		halfScreen = Screen.width / 2;
+		*/
     }
 
     void Update()
@@ -39,7 +40,7 @@ public class PlayerManager : MonoBehaviour
 
         // check if we are running either in the Unity editor or a pc build
 
-		#if UNITY_STANDALONE || UNITY_EDITOR
+		#if UNITY_STANDALONE || UNITY_EDITOR 
 
         // 3 - Retrieve the mouse position
 		if (Input.GetMouseButton(0))
@@ -50,14 +51,10 @@ public class PlayerManager : MonoBehaviour
 			//print("mouse click pos: " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
 
 			// check if greater thatn half the screen. move left if less than and right if greater than.
-			if (clickPos.x < 1f){
-				print("PLAYER SIZE: " + gameObject.GetComponent<BoxCollider2D>().size);
-				print("PLAYER POS: " + gameObject.transform.position);
-				print("--------------------------------------------------------------");
+			if (clickPos.x < 1f){				
 				gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
 			}
 			if (clickPos.x > 1f) {
-				print("move right");
 				gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
 			}
 				
@@ -65,16 +62,17 @@ public class PlayerManager : MonoBehaviour
 
 		// check if mobile or not and if it is the use this input.
 		// this checks IOS, ANDROID, and WINDOWS mobile OS type.
-		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8
+		#elif  UNITY_ANDROID || UNITY_IOS
 
         // Check if Input has registered more than zero touches
+
 		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary){
 			Vector2 touchPosition = Input.GetTouch(0).position;
 
 			// Check if it is left or right and go in that direction
-			if (touchPosition.x < halfScreen){
+			if (touchPosition.x < 1f){
 				gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
-			} else if(touchPosition.x > halfScreen){
+			} else if(touchPosition.x > 1f){
 				gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
 			}
 		}
