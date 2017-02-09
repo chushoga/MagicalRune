65039f239ad2669cb2281c4433a0e8026679bbfa
chushoga <chushoga@gmail.com>
2017-02-10 00:32:45 +0900

    Fixed instatiating buttons
    TODO: hook to pickups

--- Assets/Scripts/Monobehaviours/Pickups/PickupManager.cs
+++ Assets/Scripts/Monobehaviours/Pickups/PickupManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PickupManager : MonoBehaviour {
 
-	[SerializeField]
-	private float minorHealth = 10;
+	[SerializeField] private float minorHealth = 10;
+	[SerializeField]private GameObject button;
+	[SerializeField]private GameObject inventoryBar;
+
+	void Update() {
+		if (Input.GetKeyDown ("space")) {
+			print ("space key was pressed");
+			GameObject newButton = Instantiate (button) as GameObject;
+			newButton.transform.SetParent (inventoryBar.transform, false);
+		}
+	}
 
 	// MINOR HEALTH
 	public void HealthMinor(){
 		// ADD A MINOR AMOUNT OF HEALTH
 		// TRIGGER ADD HEALTH SPARKLE EFFECT HERE.
 		Debug.Log("minorHeath: " + minorHealth);
+
 	}
+
+
 }
