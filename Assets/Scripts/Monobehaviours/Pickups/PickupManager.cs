6e0ef2848b0edcf9c6de53548130c9bfcff9fb16
chushoga <chushoga@gmail.com>
2017-02-09 13:07:33 +0900

    Worked on Item buttons. Pickups starting to work well.
    TODO: add the buttons dynamically.

---
+++ Assets/Scripts/Monobehaviours/Pickups/PickupManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour {
+
+	[SerializeField]
+	private float minorHealth = 10;
+
+	// MINOR HEALTH
+	public void HealthMinor(){
+		// ADD A MINOR AMOUNT OF HEALTH
+		// TRIGGER ADD HEALTH SPARKLE EFFECT HERE.
+		Debug.Log("minorHeath: " + minorHealth);
+	}
+}
